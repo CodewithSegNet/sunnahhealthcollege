2f90325183419ab24f3a83815cce020b7bc88a79
segunUb18 <eeomo@yahoo.com>
2023-11-10 16:03:00 +0100

    :art: add the course table

---
+++ app/models/course_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Import
+from app.app import db
+from app.department_model import Department
+
+class Course(db.Model):
+    '''
+    A class that defines the Course Description
+    '''
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('Department.departments.id'), nullable=False)
+    course_title = db.Column(db.String(255), nullable=False)
+    course_code = db.Column(db.String(20), nullable=False, unique=True)
+    
+
+    # Define a relationship with the Department model
+    department = db.relationship('Department', backref=db.backref('courses', lazy=True))
+
+    # Constructor to initialize Course Object with attributes.
+    def __init__(self, department_id, course_title, course_code):
+        self.department_id = department_id
+        self.course_title = course_title
+        self.course_code = course_code
--- app/models/student_model.py
+++ app/models/student_model.py
@@ -7,6 +7,17 @@ from app.app import db
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from app.department_model import Department
+import course_model 
+
+
+
+# Association Table for the many-to-many relationship
+student_courses_association = db.Table('student_courses_association',
+    db.Column('admission_number', db.Integer, db.ForeignKey('students.admission_number')),
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id')),
+    db.Column('course_title', db.String(255), nullable=True),
+)
+
 
 
 class Student(db.Model):
@@ -18,7 +29,7 @@ class Student(db.Model):
     name = db.Column(db.String(255), nullable=False)
     date_of_birth = db.Column(db.Date, nullable=False)
     department_id = db.Column(db.Integer, db.ForeignKey('Department.department_id'), nullable=False)
-    department_level = db.Column(db.String(3), nullable=False)
+    department_level = db.Column(db.Integer(3), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     phone_number = db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
@@ -29,15 +40,20 @@ class Student(db.Model):
     role = db.relationship('Role', backref=db.backref('students', lazy=True))
 
 
+     # Define a relationship with the Course model through the association table
+    courses = db.relationship('Course', secondary=student_courses_association, backref=db.backref('students', lazy=True))
+
+
     # Constructor to initialize Student Object with attributes.
-    def __init__(self, name, date_of_birth, department_id, department_level, email, phone_number, role_id):
+    def __init__(self, name, date_of_birth, department_id, department_level, email, phone_number):
         self.name = name
         self.date_of_birth = date_of_birth
         self.department_id = department_id
         self.department_level = department_level
         self.email = email
         self.phone_number = phone_number
-        self.role_id = role_id
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
     
 
 
