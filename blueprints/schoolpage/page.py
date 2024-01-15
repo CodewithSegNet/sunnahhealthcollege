fe28a385b69098ceacfc80340848f91f04222c48
segunUb18 <eeomo@yahoo.com>
2024-01-15 22:39:24 +0100

    :zap: testcase

--- blueprints/schoolpage/page.py
+++ blueprints/schoolpage/page.py
@@ -6,7 +6,7 @@ from models.image import Image
 from app import cache, db
 from functools import wraps
 import requests
-from urllib.parse import quote_plus
+from urllib.parse import quote, unquote
 import jwt
 from io import BytesIO
 from werkzeug.utils import secure_filename
@@ -347,10 +347,10 @@ def get_image():
     
     if admission_number:
         # Encode the admission_number parameter
-        encoded_admission_number = quote_plus(admission_number)
+        decoded_admission_number = unquote(admission_number)
 
         # Retrieve the latest image associated with the student
-        image = Image.query.filter_by(student_admission_number=encoded_admission_number).order_by(Image.created_at.desc()).first()
+        image = Image.query.filter_by(student_admission_number=decoded_admission_number).order_by(Image.created_at.desc()).first()
 
         if image and image.image_data:
             # Send the image data to the client
