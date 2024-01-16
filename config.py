c19d110b0ac9525119960facef13efd18bedfe03
segunUb18 <eeomo@yahoo.com>
2024-01-16 13:27:32 +0100

    :pencil2: fix typos

--- config.py
+++ config.py
@@ -25,13 +25,13 @@ class Config:
 
     # SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}/{os.getenv('DATABASE')}"
 
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}/{os.getenv('DATABASE')}?charset=utf8mb4&ssl_ca=/etc/ssl/certs/ca-certificates.crt"
+    # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}/{os.getenv('DATABASE')}?charset=utf8mb4&ssl_ca=/etc/ssl/certs/ca-certificates.crt"
 
     # Disable track modifictions to avoid warning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    ssl_mode="VERIFY_IDENTITY",
-    ssl={ "ca": "" }
+    # ssl_mode="VERIFY_IDENTITY",
+    # ssl={ "ca": "" }
 
 
     # Define SSL options in engine options
